5062238cdfe86be5458f66d614501d5712d6ba54
AndreaGasparMiramontes <142550697+AndreaGasparMiramontes@users.noreply.github.com>
2024-06-28 19:59:55 -0600

    My time using openai api expired :( but this works

---
+++ functions.py
@@ -0,0 +1,38 @@
+from pynput.keyboard import Key, Controller
+import time
+
+keyboard = Controller()
+
+def get_weather():
+    return("38")
+
+def pause():
+    global keyboard
+    keyboard.press(Key.media_play_pause)
+    return("True")
+
+def next():
+    global keyboard
+    keyboard.press(Key.media_next)
+    return("True")
+
+def previous():
+    global keyboard
+    keyboard.press(Key.media_previous)
+    return("True")
+
+def volume_down():
+    global keyboard
+    for a in range (5):
+        keyboard.press(Key.media_volume_down)
+        time.sleep(1)
+        keyboard.release(Key.media_volume_down) 
+    return("True")
+
+def volume_up():
+    global keyboard
+    for a in range (5):
+        keyboard.press(Key.media_volume_up)
+        time.sleep(1)
+        keyboard.release(Key.media_volume_up) 
+    return("True")
